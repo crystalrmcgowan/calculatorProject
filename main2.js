c1d8bba2de1ce869ffb77da5ea3fa89cf9055820
Crystal <crystal.r.mcgowan@gmail.com>
2017-07-23 16:18:24 -0400

    Everything equals 0

--- main2.js
+++ main2.js
@@ -1,71 +1,46 @@
-let buttons = document.querySelectorAll("button")
-let clear = document.querySelector("#cButton")
-let display = document.querySelector("output")
-let decimal = document.querySelector("#decimal")
-let equals = document.querySelector("#equal")
 let numbers = document.querySelectorAll(".numbers")
-let operations = document.querySelectorAll(".operations")
+let output = document.querySelector("output")
+let clear = document.querySelectorAll("#cButton")
+let opsButtons = document.querySelectorAll(".operations")
+let equals = document.querySelectorAll("#equal")
 
-let num1 = ''
-let num2 = ''
-let op = ''
+let num1 = 0
+let num2 = 0
+let results = 0
 
-const seven = document.querySelector("#seven")
-const eight = document.querySelector("#eight")
-const nine = document.querySelector("#nine")
-const four = document.querySelector("#four")
-const five = document.querySelector("#five")
-const six = document.querySelector("#six")
-const one = document.querySelector("#one")
-const two = document.querySelector("#two")
-const three = document.querySelector("#three")
-
-for (var i = 0; i < operations.length; i++) {
-  operations[i].addEventListener("click", function(event){
-    if (operations.length === 0) {
-      num1 += numbers
-    } else {
-      num2 += numbers
-    }
-    // set `op` to be the value of this button that was clicked
+clear.forEach((button, i) => {
+  button.addEventListener('click', () => {
+    output.textContent = ""
   })
+})
+
+function addValueToDisplay(event) {
+  const numbers = event.target.value
+  output.textContent += numbers
 }
 
-for (var i = 0; i < numbers.length; i++) {
-  numbers[i].addEventListener("click", function(event) {
-    num1 += event.target.value
-    display.textContent = num1
-    // OR
-    num2 += event.target.value
-    display.textContent = num2
+opsButtons.forEach((button, i) => {
+  button.addEventListener('click', addValueToDisplay)
+})
+
+numbers.forEach((button, i) => {
+  button.addEventListener('click', addValueToDisplay)
+})
+
+equals.forEach((button, i) => {
+  button.addEventListener('click', function (){
+    if (opsButtons.value === "/") {
+        results = num1 / num2
+    }
+    if (opsButtons === "*") {
+        results = num1 * num2
+    }
+    if (opsButtons === "-") {
+        results = num1 - num2
+    }
+    if (opsButtons === "+") {
+        results = num1 + num2
+    }
+    output.innerHTML = results
   })
-}
-
-for (var i = 0; i < buttons.length; i++) {
-
-  decimal.addEventListener("click", function addDecimal() {
-    display.textContent = "."
-  })
-
-  equals.addEventListener("click", function equals() {
-    if (op === "/") {
-      display.textContent = num1 / num2
-    }
-
-    if (op === "*") {
-      display.textContent = num1 * num2
-    }
-
-    if (op === "-") {
-      display.textContent = num1 - num2
-    }
-
-    if (op === "+") {
-      display.textContent = num1 + num2
-    }
-  })
-
-  clear.addEventListener("click", function clear() {
-    display.textContent = " "
-    })
-}
+})
