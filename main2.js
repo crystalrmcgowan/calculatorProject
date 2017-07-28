ab4b724e3a1a0a455a0865982038206d9ab4e20c
Crystal <crystal.r.mcgowan@gmail.com>
2017-07-27 21:48:03 -0400

    Third JS

--- main2.js
+++ main2.js
@@ -1,46 +1,43 @@
 let numbers = document.querySelectorAll(".numbers")
-let output = document.querySelector("output")
-let clear = document.querySelectorAll("#cButton")
+let output = document.querySelector("p")
+let clear = document.querySelector("#cButton")
 let opsButtons = document.querySelectorAll(".operations")
-let equals = document.querySelectorAll("#equal")
+let equals = document.querySelector("#equal")
 
 let num1 = 0
 let num2 = 0
 let results = 0
+let operator = ""
 
-clear.forEach((button, i) => {
-  button.addEventListener('click', () => {
-    output.textContent = ""
+for (var i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", () => {
+    output.innerHTML += numbers[i].textContent
   })
-})
-
-function addValueToDisplay(event) {
-  const numbers = event.target.value
-  output.textContent += numbers
 }
 
-opsButtons.forEach((button, i) => {
-  button.addEventListener('click', addValueToDisplay)
-})
-
-numbers.forEach((button, i) => {
-  button.addEventListener('click', addValueToDisplay)
-})
-
-equals.forEach((button, i) => {
-  button.addEventListener('click', function (){
-    if (opsButtons.value === "/") {
-        results = num1 / num2
-    }
-    if (opsButtons === "*") {
-        results = num1 * num2
-    }
-    if (opsButtons === "-") {
-        results = num1 - num2
-    }
-    if (opsButtons === "+") {
-        results = num1 + num2
-    }
-    output.innerHTML = results
+for (var i = 0; i < opsButtons.length; i++) {
+  opsButtons[i].addEventListener("click", function() {
+    operator = opsButtons[i].textContent
+    num1 = parseFloat(output.textContent)
+    output.textContent = ""
   })
+}
+
+clear.addEventListener("click", function clearIt() {
+  output.innerHTML = ""
+})
+
+equals.addEventListener("click", function dothings(event) {
+  num2 = parseFloat(output.textContent)
+  output.innerHTML = ""
+  if (op === "/") {
+    results = num1 + num2
+  } else if (op === "*") {
+    results = num1 * num2
+  } else if (op === "-") {
+    results = num1 - num2
+  } else if (op === "+") {
+    results = num1 + num2
+  }
+  output.innerHTML = results
 })
