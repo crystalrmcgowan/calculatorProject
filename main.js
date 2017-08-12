7e368388b78487b3d73859f5540d2710852a330c
Crystal <crystal.r.mcgowan@gmail.com>
2017-08-12 17:18:56 -0400

    Cleaned up HTML/CSS/JS

--- main.js
+++ main.js
@@ -1,59 +1,49 @@
-let buttons = document.querySelectorAll("button")
-let value = document.querySelectorAll("value")
-let result = document.querySelector("output")
-
+const numbers = document.querySelectorAll(".numbers")
+const operations = document.querySelectorAll(".operations")
+const displayBox = document.querySelector("output")
+const clearButton = document.getElementById("cButton")
+const equals = document.getElementById("equal")
 
 let num1 = 0
 let num2 = 0
-let op = ''
+let operator = ""
 
-//I want you to run through all of the buttons
-for (var i = 0; i < buttons.length; i++) {
-  if (buttons[i].type === "reset") {
-    buttons[i].addEventListener("click", function (){
-      document.getElementById('#result')
-      result.textContent = " "
-    })
-  }
-
-  if (buttons[i].className !== "operations") {
-    buttons[i].addEventListener("click", function() {
-      let number = this.value
-      result.textContent += number
-      if (op.length === 0) {
-        num1 += number
-      } else {
-        num2 += number
-      }
-    })
-  }
-
-  //if the button is the divide button..
-  if (buttons[i].name === "divide") {
-    let op = '/'
-    //I want you to do something when the user clicks
-    buttons[i].addEventListener("click", function(){
-      //the thing I want you to do is to take the user's first input and divide it by the user's second input
-      document.getElementById('#divide')
-      result.textContent = num1 / num2
-    })
-  }
-
-  if (buttons[i].name === "multiply") {
-    buttons[i].addEventListener("click", function(){
-      let op = '*'
-    })
-  }
-
-  if (buttons[i].name === "subtract") {
-    buttons[i].addEventListener("click", function() {
-      let op = '-'
-    })
-  }
-
-  if (buttons[i].name === "add") {
-    buttons[i].addEventListener("click", function() {
-      let op = '+'
-    })
-  }
+for (var i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", event => {
+    let numberValue = event.target.value
+    displayBox.textContent += numberValue
+    if (operator) {
+      num2 += numberValue
+    } else {
+      num1 += numberValue
+    }
+  })
 }
+for (var i = 0; i < operations.length; i++) {
+  operations[i].addEventListener("click", operationsEvent => {
+    operator = operationsEvent.target.value
+    displayBox.textContent += operator
+
+    if (operations[i] === "=") {
+      let answer = ""
+      console.log("hey")
+    }
+  })
+}
+clearButton.addEventListener("click", clearEvent => {
+  num1 = ""
+  num2 = ""
+  operator = ""
+  displayBox.textContent = ""
+})
+equals.addEventListener("click", answerEvent => {
+  if (event.target.value === "/") {
+    answer = num1 / num2
+  } else if (event.target.value === "*") {
+    answer = num1 * num2
+  } else if (event.target.value === "-") {
+    answer = num1 - num2
+  } else if (event.target.value === "+") {
+    answer = num1 + num2
+  }
+})
